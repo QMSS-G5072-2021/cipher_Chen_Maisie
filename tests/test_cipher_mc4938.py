dd4180bd48d1e9be883b69d4b61093295f385067
MaisieChen <77697223+MaisieChen@users.noreply.github.com>
2021-11-11 18:41:02 -0500

    3. Add tests
    a) Add one of the tests from last week's homework exercise into the test folder of your package. Add pytest as a dev dependency of your package and make sure pytest is able to complete the test successfully. Commit this change to git.

--- tests/test_cipher_mc4938.py
+++ tests/test_cipher_mc4938.py
@@ -1,1 +1,12 @@
 from cipher_mc4938 import cipher_mc4938
+import pandas as pd
+
+def test_cipher_symbol(example):
+    alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    for c in example[0]:
+        index = alphabet.find(c)
+        if index == -1:
+            expected = cipher(example[0],example[1])
+            assert expected == example[2], 'Result does not match the expected value'
+    
+test_cipher_symbol(test_word)
